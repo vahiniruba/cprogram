791cbf9739d433cb435a93beabad5bc7e458d525
vahiniruba <vahini.sethuraman95@gmail.com>
2017-06-23 09:36:34 +0530

    Add files via upload

---
+++ POSNEG.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ clrscr();
+ printf("\nEnter the given number");
+ scanf("%d",&a);
+ if(a>0)
+ printf("\nThe given number is positive");
+ else
+  printf("\nThe given number is negative");
+  getch();
+}
